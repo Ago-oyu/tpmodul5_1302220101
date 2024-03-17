9f1faa6b7ab5ad7714095171e02cce157e30cfc7
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2024-03-17 19:09:17 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302220101/Program.cs
+++ tpmodul5_1302220101/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302220101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGeneric<int> data = new DataGeneric<int>(1302220101);
+            data.PrintData();
+        }
+    }
+
+}
